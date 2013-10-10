f1062ec7784894d6de27f6e9013a9d8293a6089a
Alexis <Alexis@ALEXIS-PC>
2013-10-11 01:26:48 +0900

    - Try to make a windows on java

--- Java_OOP_team_project/src/UI/GameInterface.java
+++ Java_OOP_team_project/src/UI/GameInterface.java
@@ -1,10 +1,23 @@
 package UI;
 
-public class GameInterface {
+import javax.swing.JFrame;
 
+public class GameInterface extends JFrame {
+
+	private static int WIDTH =  300 ;
+	private static int HEIGHT = 200 ;
+	
 	public GameInterface() {
 		super();
-		// TODO Auto-generated constructor stub
+		System.out.println("Start Game Interface");
+		this.startInterface();
+	}
+	
+	public void startInterface(){
+		this.setSize(WIDTH, HEIGHT);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
 	}
 	
 	
--- Java_OOP_team_project/src/main/Game.java
+++ Java_OOP_team_project/src/main/Game.java
@@ -1,10 +1,14 @@
 package main;
 
+import UI.*;
+
 public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		GameInterface game = new GameInterface() ;
+		
+		
 	}
 
 }
