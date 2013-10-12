9756277b2b00b3e688a4f3fe72e7e51587ab3560
Alexis <Alexis@ALEXIS-PC>
2013-10-12 19:10:16 +0900

    Add Class Settings

--- Java_OOP_team_project/src/GameInterface/GameInterface.java
+++ Java_OOP_team_project/src/GameInterface/GameInterface.java
@@ -1,5 +1,17 @@
 package GameInterface;
 
+import Settings.* ;
+
 public class GameInterface {
+	
+	Settings set ;
+	
+	public GameInterface(Settings settings) {
+		super();
+		set = settings;
+		set.setGameTime(10);
+	}
+	
+	
 
 }
---
+++ Java_OOP_team_project/src/Settings/Settings.java
@@ -0,0 +1,29 @@
+package Settings;
+
+public class Settings {
+	
+	private int gameTime = 0 ; // In second
+	private int maximumRoud = 0 ;
+	private int maximumPoint = 0 ;
+	public int getGameTime() {
+		return gameTime;
+	}
+	public void setGameTime(int gameTime) {
+		this.gameTime = gameTime;
+	}
+	public int getMaximumRoud() {
+		return maximumRoud;
+	}
+	public void setMaximumRoud(int maximumRoud) {
+		this.maximumRoud = maximumRoud;
+	}
+	public int getMaximumPoint() {
+		return maximumPoint;
+	}
+	public void setMaximumPoint(int maximumPoint) {
+		this.maximumPoint = maximumPoint;
+	}
+
+	
+	
+}
